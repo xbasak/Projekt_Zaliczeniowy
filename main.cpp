930883f455cf3683ecb87f9ede319fd97784f09c
Arkadiusz Basak <s99490@pollub.edu.pl>
2023-06-04 22:42:27 +0200

    Start

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"NR\tFUNKCJA"<<endl
+       <<"1)\tDodaj klienta"<<endl
+      <<"2)\tModyfikuj istniejacego klienta"<<endl
+     <<"3)\tWyswietl dostepne produkty"<<endl
+    <<"4)\tZloz zamowienie"<<endl
+    <<"5)\tEdytuj zamowienie"<<endl
+    <<"6)\tWyswietl liste zamowien"<<endl
+    <<"7)\tZapisz dane klientow do plikow"<<endl
+    <<"8)\tZapisz zamowienia do plikow"<<endl;
+      return 0;
+}
